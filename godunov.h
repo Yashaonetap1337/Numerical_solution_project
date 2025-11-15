7c32e95789a16f24ca930a057cb09a4f010890ee
Alisa74-dot <kukuskina2004@gmail.com>
2025-11-15 09:38:31 +0300

    Update godunov.h

--- godunov.h
+++ godunov.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "types.h"
 
-void godunov_step(Grid& grid, double dt, const Config& cfg);
+void godunov_step(Grid& grid, double dt, const Config& cfg, std::vector<Flux>& fluxes);
